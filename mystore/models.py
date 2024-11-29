1bc2ba1d611296b5b0b8deef63993bafad81499a
DatNT1507 <nguyenthanhdat15072004@gmail.com>
2024-11-29 15:05:26 +0700

    create table brands

--- mystore/models.py
+++ mystore/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class brands(models.Model):
+    brand_id = models.CharField(max_length=100, primary_key=True)
+    brand_title = models.CharField(max_length=100)
--- mystore/views.py
+++ mystore/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from mystore.models import brands
+
+
 def test(request):
+    brand = brands.objects.all().values()
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    context = {
+        'brand': brand,
+    }
+    return HttpResponse(template.render(context, request))
