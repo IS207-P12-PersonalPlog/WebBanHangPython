02fcda250d7debd728a1728edcbd049bb7504c88
DatNT1507 <nguyenthanhdat15072004@gmail.com>
2024-11-29 15:07:42 +0700

    create table categories

--- mystore/models.py
+++ mystore/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class brands(models.Model):
     brand_id = models.CharField(max_length=100, primary_key=True)
     brand_title = models.CharField(max_length=100)
+
+class categories(models.Model):
+    category_id = models.CharField(max_length=100, primary_key=True)
+    category_title = models.CharField(max_length=100)
