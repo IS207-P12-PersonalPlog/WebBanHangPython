81ea129c62747af29d93a13ab44d83487698d626
DatNT1507 <nguyenthanhdat15072004@gmail.com>
2024-12-06 10:42:54 +0700

    modify model

--- mystore/models.py
+++ mystore/models.py
@@ -3,37 +3,37 @@ from django.db import models
 # Create your models here.
 #Chua biet dat the ngoai
 class useraccount(models.Model):
-    user_id = models.CharField(auto_created=True, max_length=4)
+    user_id = models.CharField(auto_created=True, max_length=4, primary_key=True)
     user_password = models.CharField(max_length=40)
     ho_ten = models.CharField(max_length=40, default="Nguyen Van A")
-    so_dt = models.CharField(max_length=10, default=None)
+    so_dt = models.CharField(max_length=10, default=None, blank=True)
     allow_edit = models.BooleanField(default=False)
 
 class brands(models.Model):
     brand_id = models.CharField(max_length=100, primary_key=True)
     brand_title = models.CharField(max_length=100)
 
-class categories(models.Model):
-    category_id = models.CharField(max_length=100, primary_key=True)
-    category_title = models.CharField(max_length=100)
+# categories(models.Model):
+    #category_id = models.CharField(max_length=100, primary_key=True)
+    #category_title = models.CharField(max_length=100)
 
 class sp(models.Model):
-    MASP = models.CharField(max_length=4, primary_key=True, auto_created=True)
-    TENSP = models.CharField(max_length=100)
-    DVT = models.CharField(max_length=30, default=1)
-    NUOCSX = models.CharField(max_length=100, default="China")
-    GIA = models.IntegerField(default=0)#Chua biet co kieu money hay ko
-    brand_id = models.CharField(max_length=100)#
-    category_id = models.CharField(max_length=100)#
-    HINHANH = models.CharField(max_length=100)
+    masp = models.CharField(max_length=4, primary_key=True, auto_created=True)
+    tensp = models.CharField(max_length=100)
+    dvt = models.CharField(max_length=30, default=1)
+    nuocsx = models.CharField(max_length=100, default="China")
+    gia = models.IntegerField(default=0)#Chua biet co kieu money hay ko
+    brand_id = models.ForeignKey(brands, on_delete=models.CASCADE)#
+    #category_id = models.CharField(max_length=100)#
+    hinhanh = models.ImageField(upload_to='images/')
 
 class hoadon(models.Model):
-    SOHD = models.IntegerField(primary_key=True, auto_created=True)
-    NGHD = models.DateTimeField()
-    user_id = models.CharField(max_length=4)#
-    TRIGIA = models.IntegerField()#Chua biet co kieu money hay ko
+    sohd = models.IntegerField(primary_key=True, auto_created=True)
+    nghd = models.DateTimeField()
+    user_id = models.ForeignKey(useraccount, on_delete=models.CASCADE)#
+    trigia = models.IntegerField()#Chua biet co kieu money hay ko
 
 class cthd(models.Model):
-    SOHD = models.IntegerField()#
-    MASP = models.CharField(max_length=4) #
-    SL = models.SmallIntegerField()
+    sohd = models.ForeignKey(hoadon, on_delete=models.CASCADE)#
+    masp = models.ForeignKey(sp, on_delete=models.CASCADE) #
+    sl = models.SmallIntegerField()
