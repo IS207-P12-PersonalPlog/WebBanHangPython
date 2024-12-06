5625cd80ffaf479f8fd59bb62faa65216464f13c
DatNT1507 <nguyenthanhdat15072004@gmail.com>
2024-12-06 10:48:50 +0700

    modify admin

--- mystore/admin.py
+++ mystore/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(useraccount)
+admin.site.register(brands)
+admin.site.register(sp)
+admin.site.register(hoadon)
+admin.site.register(cthd)
