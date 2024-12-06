481fcb7ec7a5293b0a51a6df03f0b9b792886933
DatNT1507 <nguyenthanhdat15072004@gmail.com>
2024-12-06 12:17:48 +0700

    load image but not yet complete

--- mystore/urls.py
+++ mystore/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+#load image
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('test/', views.test, name='test'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+#load image
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
